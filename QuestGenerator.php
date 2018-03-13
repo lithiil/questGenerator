f32e33223f2dcbc2945eabe04046a145ab2f13bb
lithiil <tudorjamal@gmail.com>
2018-03-13 23:23:16 +0200

    Added newline after output and content for data

--- QuestGenerator.php
+++ QuestGenerator.php
@@ -3,12 +3,14 @@
 class QuestGenerator
 {
     public function getData () {
+
         $rawData = file_get_contents('data/data.json');
         $data = json_decode($rawData);
         return $data;
     }
 
     public function generateQuest ($data) {
+
         $ids = [
             $subjectId = rand(1, count($data->subjects)) - 1,
             $nameId = rand(1, count($data->names)) - 1,
@@ -21,7 +23,7 @@ class QuestGenerator
         ];
 
         $quest = 'The ' . $data->adjective[$adjectiveId] . ' ' . $data->races[$raceId] . ' ' .  $data->subjects[$subjectId] . ' named ' . $data->names[$nameId] . ' ,' . $data->subjectAction[$subjectActionId]
-            . ' ' . $data->subjectItem[$subjectItemId] . ' ' . $data->subjectLocation[$subjectLocationId] . ' . ' . $data->subjectStatus[$subjectStatusId];
+            . ' ' . $data->subjectItem[$subjectItemId] . ' ' . $data->subjectLocation[$subjectLocationId] . ' . ' . $data->subjectStatus[$subjectStatusId].PHP_EOL;
 
         return $quest;
     }
