1d85e6e67f2aef4745b3b57ed94ddb12c0be1ecb
tpalade <tpalade@bitdefender.com>
2018-04-26 14:44:28 +0300

    modified the scrapper

--- tools/generatorScraper.php
+++ tools/generatorScraper.php
@@ -2,11 +2,13 @@
 /**
  * This script scrapes https://citygenerator.morgajel.net
  * It is not meant to be used for any type of commercial use
+ * If you are the owner of this site and you are not ok with the using of this script email me and I will remove it
  */
 
 require '../simple_html_dom.php';
 
-function scrapeGenerator ($url, $selector) {
+function scrapeGenerator($url, $selector)
+{
 
     $results = [];
 
@@ -15,14 +17,28 @@ function scrapeGenerator ($url, $selector) {
     $items = $html->find($selector);
 
     foreach ($items as $item) {
-        array_push($results,$item->innertext);
+
+        $result = $item->innertext;
+
+        if (strpos($result, '<h2>Summary</h2>')) {
+            $result = trim(str_replace('<h2>Summary</h2>', '', $result));
+        }
+        array_push($results, $result);
     }
     return $results;
 }
 
 
 //Exmaples:
-print_r(scrapeGenerator('https://citygenerator.morgajel.net/postinggenerator', '#postings li'));
-print_r(scrapeGenerator('https://citygenerator.morgajel.net/rumorgenerator', '#rumors li'));
-print_r(scrapeGenerator('https://citygenerator.morgajel.net/legendgenerator', '#legend li'));
-print_r(scrapeGenerator('https://citygenerator.morgajel.net/bondgenerator', '#bonds li'));
+// Generate Quest Post Sign
+//print_r(scrapeGenerator('https://citygenerator.morgajel.net/postinggenerator', '#postings li'));
+// Generate Rumors
+//print_r(scrapeGenerator('https://citygenerator.morgajel.net/rumorgenerator', '#rumors li'));
+//Generate Legends
+//print_r(scrapeGenerator('https://citygenerator.morgajel.net/legendgenerator', '#legend li'));
+//Generate Bonds
+//print_r(scrapeGenerator('https://citygenerator.morgajel.net/bondgenerator', '#bonds li'));
+//Generate Magic Item
+//print_r(scrapeGenerator('https://citygenerator.morgajel.net/magicitemgenerator', '#summary'));
+
+
